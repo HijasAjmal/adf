43c693e954d4c2b9134f939c2b07df315c1bfdc2
vrtaquest <rbcode@imap.cc>
2018-05-16 23:48:33 +0530

    The first csv reader and core population procedures

---
+++ adf/readers.py
@@ -0,0 +1,16 @@
+import csv
+
+"""
+Contains record yielders compatible with core.Adf
+Basically a independant record unit as a dict
+"""
+
+def read_csv(filename):
+    """
+    Reads a csv and yields a record one dict at a time
+    """
+    with open(filename) as f:
+        reader = csv.DictReader(f)  # Eh, simple enough!
+        for row in reader:
+            # The generator should help with the memory management somewhat
+            yield row
